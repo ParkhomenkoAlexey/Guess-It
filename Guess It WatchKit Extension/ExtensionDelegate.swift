41bcbae4f4e8cab9073267f229fd43dcf522d096
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-04-17 17:38:28 +0500

    Initial Commit

---
+++ Guess It WatchKit Extension/ExtensionDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  ExtensionDelegate.swift
+//  Guess It WatchKit Extension
+//
+//  Created by Алексей Пархоменко on 17.04.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import WatchKit
+
+class ExtensionDelegate: NSObject, WKExtensionDelegate {
+
+    func applicationDidFinishLaunching() {
+        // Perform any final initialization of your application.
+    }
+
+}
